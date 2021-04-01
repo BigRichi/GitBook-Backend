2aebc8ccf0e83384e62acbad436e367cb8616b38
BigRichi <big.richi@outlook.com>
2021-03-31 22:41:45 -0400

    end of wednesday

--- app/models/git_user.rb
+++ app/models/git_user.rb
@@ -8,6 +8,70 @@ class GitUser < ApplicationRecord
         self.hireable = "N/A" if self.hireable.nil?
     end
 
+    def total_watchers
+        repositories.sum{|r| r.watchers}
+    end 
+
+    def average_watchers
+        total_watchers/self.repositories
+    end
+    
+    def total_stargazers
+        repositories.sum{|r| r.stargazers_count}
+    end 
+
+    def average_stargazers
+        total_stargazers/self.repositories.size
+    end
+    
+    def total_commits
+        repositories.sum{|r| r.commits}
+    end 
+
+    def average_commits
+        total_commits/self.repositories.size
+    end
+    
+    def total_repo_size
+        repositories.sum{|r| r.commits}
+    end 
+
+    def average_repo_size
+        total_commits/self.repositories.size
+    end
+
+    def size_of_repos
+        repository_size = {
+            small_scale: 0,
+            medium_scale: 0,
+            large_scale: 0
+        }
+        
+        repositories.each do |repository|
+            if repository.size < 100
+                repository_size[:small_scale] += 1
+            elsif repository.size > 100 && repository.size < 1000
+                repository_size[:medium_scale] += 1
+            else
+                repository_size[:large_scale] += 1
+            end
+        end               
+        return repository_size
+    end
+    
+
+    def repo_language
+        language = {}
+        repositories.each do |repository|
+            if language.keys.include?(repository.language)
+                language["#{repository.language}"] += 1
+            else
+                language["#{repository.language}"] = 1
+            end
+        end
+        return language
+    end
+
 
 end
 
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -12,21 +12,3 @@ class Repository < ApplicationRecord
     self.description = "N/A" if self.description.nil?
   end
 end
-# new = {
-# :git_user_id: 1,
-# :repo_id: 324324213423,
-# :name: "js-events-mini-challenge",
-# :description: nil,
-# :html_url: "https://github.com/jpham1109/js-events-mini-challenge",
-# :language: "JavaScript",
-# :size: 6627,
-# :forks_count: 0,
-# :commits: 2,
-# :events_url: "https://api.github.com/repos/jpham1109/js-events-mini-challenge/events",
-# :last_commit_date: Wed, 17 Mar 2021 18:43:57.000000000 UTC +00:00,
-# :watchers: nil,
-# :stargazers_count: nil,
-# :repo_creation: Wed, 17 Mar 2021 17:19:53.000000000 UTC +00:00,
-# :created_at: Wed, 31 Mar 2021 22:38:11.497668000 UTC +00:00,
-# :updated_at: Wed, 31 Mar 2021 22:38:11.497668000 UTC +00:00
-# }
